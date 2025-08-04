89c4ee5267e2cf3955fd30d57bd3e9fb9356aacb
Abishaï Yapélé <abishaiyp@gmail.com>
2025-08-04 14:12:01 +0000

    Changer le titre de la page HTML

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Site from "./Site/Site";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Site />
   </StrictMode>
 )
