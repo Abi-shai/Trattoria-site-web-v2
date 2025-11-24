e26206df18b49d76bb508046a8996eb13bd10528
Abishaï Yapélé <abishaiyp@gmail.com>
2025-11-24 10:26:03 +0000

    chore: Reorder 'Sans gluten' and 'Autres' in pizza supplement options.

--- src/menu-data.js
+++ src/menu-data.js
@@ -1416,7 +1416,7 @@ export const specialiData = [
 
 export const pizzaMoreInfos = {
   suppléments: [
-    "Mozzarella de bufflonne", "Burrata", "Crudo ou speck", "Bresaola", "Cèpes (Porcini)", "Pleurotes grises (Funghi orecchiette)", "Mozzarellas cerises", "Autres", "Sans gluten"
+    "Mozzarella de bufflonne", "Burrata", "Crudo ou speck", "Bresaola", "Cèpes (Porcini)", "Pleurotes grises (Funghi orecchiette)", "Mozzarellas cerises", "Sans gluten", "Autres"
   ],
   notes: [
     "* Possibilité halal", "** Disponible en stock selon arrivage"
