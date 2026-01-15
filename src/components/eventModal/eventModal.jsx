1ac792098bfa894a0d0d8f35f02729d4c215858b
Abishaï Yapélé <abishaiyp@gmail.com>
2026-01-15 09:44:41 +0000

    changing the current event image to a lighter version

--- src/components/eventModal/eventModal.jsx
+++ src/components/eventModal/eventModal.jsx
@@ -9,7 +9,7 @@ import EventImage from '../../assets/images/current-event.jpeg';
 import './eventModal.css';
 
 
-const CURRENT_EVENT_ID = 'event-proposition-janvier-2026';
+const CURRENT_EVENT_ID = 'event-propositions-janvier-2026';
 const DISPLAY_DELAY = 3000;
 
 const EventModal = () => {
